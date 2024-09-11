66d178357bdf21c4aca4d7ef1305cf0509444357
Tharusha <156021977+CodeMaestro2001@users.noreply.github.com>
2024-09-11 12:41:06 +0530

    create roter

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const dotenv=require("dotenv").config();
 const app=express();
 const port=process.env.PORT || 5000;
 
-app.get('/api/contacts', (req,res) =>{
-   res.json({message:"i will win thids race"})
-});
 
+
+app.use("/api/contacts", require("./Routes/contactRouter"));
 
 
 app.listen(port, () =>{
